3093c9e4d0f0f36bea6fc912848cedc51352f517
Guillaume DOUSSET <guillaume.dousset@etu.univ-nantes.fr>
2015-12-07 18:14:27 +0100

    Update moveHoraire() : - ajout de commentraires et rectification du code (vérifié et testé et donc approuvé) pour la rotation des couronnes pour les mouvements normalisés "F" et "B".

--- utils.py
+++ utils.py
@@ -55,6 +55,7 @@ class Cube:
         if (type(mouvement) == str) and (len(mouvement) == 1) :
             # récupérer la face associée au mouvement :
             idFace = dico[mouvement.upper()]
+            # pour une meilleure lisibilité du code :
             face = np.copy(self.L[idFace])
 
             # effectuer la rotation d'1/4 de tour horaire sur la face         
@@ -65,7 +66,7 @@ class Cube:
 
             # les colonnes de la face deviennent ses nouvelles lignes après
             # rotation :
-            #face = np.array([colonne_1, colonne_2, colonne_3]) # je ne comprends pas pourquoi l'aliasing ne fonctionne pas
+            ##face = np.array([colonne_1, colonne_2, colonne_3]) # je ne comprends pas pourquoi l'aliasing ne fonctionne pas
             self.L[idFace] = np.array([colonne_1, colonne_2, colonne_3]) # ça fonctionne
 
             # Pour les autres faces entrainées dans la rotation :
@@ -98,43 +99,76 @@ class Cube:
                     
             # si ce sont les faces F ou B :
             elif idFace == 2 or idFace == 4 :
+            # sur les faces UP & DOWN, il faudra déplacer des lignes uniquement,
+            # soit la première soit la dernière en fonction de si la rotation
+            # est F ou B et en fonction de si la face concernée est UP ou DOWN.
+            # D'où idFirstRow et idSecondRow qui correspondent à ces indices
+            # de ligfne "changeant".
+            # De même pour idFirstColumn et idSecondColumn.
+            # C'est aussi ce qui empêche d'automatiser le processus avec une
+            # boucle for.
                 if idFace == 2:
+                    # l'indice de la ligne de UP que l'on déplacera :
                     idFirstRow = 2
+                    # l'indice de la colonne de LEFT que l'on déplacera :
                     idFirstColumn = 2
                 else :
                     idFirstRow = 0
                     idFirstColumn = 0
 
+                # l'indice de la ligne de DOWN que l'on déplacera, qui est le
+                # complément à 2 de l'indice de la ligne déplacée de la face
+                # UP :
                 idSecondRow = 2 - idFirstRow
+                # l'indice de la colonne de RIGHT que l'on déplacera, qui est
+                # le complément à 2 de l'indice de la colonne déplacée de la
+                # face LEFT :
                 idSecondColumn = 2 - idFirstColumn
-                # En ayant arbitrairement choisi de commencer par la face 0):
-                # self.L[0][idFirstRow] # face UP
-                # self.L[5][idSecondRow] # face DOWN
-                # self.L[1][idFirstColumn] # face LEFT
-                # self.L[3][idSecondColumn] # face RIGHT
+
+                # On choisit arbitrairement de commencer la rotation par la
+                # face UP :
+                # sauvegarde de la face UP :
                 saveRow = np.copy(self.L[0][idFirstRow])
-                self.L[0][idFirstRow] = self.L[1][idFirstColumn] # face LEFT --> face UP (sens de rotation horaire)
+                # face LEFT --> face UP (sens de rotation horaire) :
+                self.L[0][idFirstRow] = [self.L[1][k][idFirstColumn] for k in \
+                                         range(-1, -4, -1)]
 
-                oldRow = self.L[3][idSecondColumn]
-                self.L[3][idSecondColumn] = saveRow # face UP --> face RIGHT (sens de rotation horaire)
+                # sauvegarde de la face RIGHT :
+                oldRow = np.copy([self.L[3][k][idSecondColumn] for k in \
+                                  range(-1, -4, -1)])
+                # face UP --> face RIGHT (sens de rotation horaire) :
+                for k in range(-1, -4, -1) :
+                    self.L[3][k][idSecondColumn] = saveRow[k] 
                 saveRow = oldRow
 
-                oldRow = self.L[5][idSecondColumn]
-                self.L[5][idSecondColumn] = saveRow # face RIGHT --> face DOWN (sens de rotation horaire)
+                # sauvegarde de la face DOWN :
+                oldRow = np.copy(self.L[5][idSecondRow])
+                # face RIGHT --> face DOWN (sens de rotation horaire) :
+                self.L[5][idSecondRow] = saveRow 
                 saveRow = oldRow
-
-                self.L[1][idSecondColumn] = saveRow # face DOWN --> face LEFT (sens de rotation horaire)           
+                
+                # face DOWN --> face LEFT (sens de rotation horaire) :
+                for k in range(-1, -4, -1) :
+                    self.L[1][k][idFirstColumn] = saveRow[k]                 
             
             # si ce sont les faces L ou R :
-            else :
-                # code
-                idRow = 0 # TEMPORAIRE, juste là pour que le else ci-dessous ne génère pas une erreur 'expected an indent block'
+            elif idFace == 1 or idFace == 3 :                         
+                # TEMPORAIRE, juste là pour que le else ci-dessous ne génère
+                # pas une erreur 'expected an indent block'
+                idRow = 0 
                 
         else :
             raise TypeError
         
 # Exemples :
+# un cube qui permet de voir le déplacement exact de chaque vignette
+# puisqu'elles sont toutes identifiées de manière unique dans la configuration
+# de départ :
 cube = Cube("123456789abcjklstuABCdefmnovwxDEFghipqryz{GHIJKLMNOPQR")
+
 cube.afficheFaces()
-cube.moveHoraire('d')
-cube.afficheFaces()
+for i in ['u', 'd', 'f', 'b', 'l', 'r']:
+    print("move = " + str(i))
+    cube.moveHoraire(i)
+    cube.afficheFaces()
+    cube = Cube("123456789abcjklstuABCdefmnovwxDEFghipqryz{GHIJKLMNOPQR")
