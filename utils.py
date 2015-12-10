a035e34aeed5dc2ca89af66b46d612b80be4ffc0
Guillaume DOUSSET <guillaume.dousset@etu.univ-nantes.fr>
2015-12-10 13:59:59 +0100

    moveHoraire() terminée et fonctionnelle !

--- utils.py
+++ utils.py
@@ -35,8 +35,10 @@ class Cube:
 
         self.L = np.array(self.L)
 
+
     def afficheFaces(self):
         print(self.L, '\n', '-----\n')
+
 	
     def moveHoraire(self, mouvement, dico = {'U':0, 'L':1, 'F':2, 'R':3, 'B':4, 'D':5}):
         """
@@ -70,7 +72,14 @@ class Cube:
             self.L[idFace] = np.array([colonne_1, colonne_2, colonne_3]) # ça fonctionne
 
             # Pour les autres faces entrainées dans la rotation :
-            # si ce sont les faces U ou D :
+
+            # /!\ la face BACK est la SEULE face qui soit inversée suivant
+            # la vue.
+            # En effet, sur une vue plane (éclatée), la première colonne de la
+            # face BACK en partant de la gauche n'est pas la première colonne
+            # de la face BACK en 3D, mais la 3ème...
+            
+            # si mouvement est U ou D :
             if idFace == 0 or idFace == 5 :
                 if idFace == 0 :
                     idRow = 0 # (NB : row = ligne) il faudra bouger toutes les premières lignes des 4 autres faces si on tourne la face UP
@@ -86,18 +95,19 @@ class Cube:
                 # Légende :
                 # idRow(x) : sélectionne la ligne d'indice idRow sur la
                 # face d'indice x
-                # x --> y : insère la ligne x dans la face y
+                # x --> y : insère la ligne x dans la face d'indice y
 
                 # On choisit arbitrairement de commencer par la face 4 :
                 saveRow = np.copy(self.L[4][idRow])
-                self.L[4][idRow] = self.L[1][idRow]
-                # puis on repète cela pour les faces 3 à 1 :
-                for i in range(3, 0, -1):
+                self.L[4][2 - idRow] = self.L[1][idRow]
+                # puis on repète cela pour les faces 3 à 1 (sens horaire) :
+                for i in range(3, 0, -1) :
                     oldRow = np.copy(self.L[i][idRow])
+                    # on conserve le même ordre entre les listes
                     self.L[i][idRow] = saveRow
                     saveRow = oldRow
                     
-            # si ce sont les faces F ou B :
+            # si mouvement est F ou B :
             elif idFace == 2 or idFace == 4 :
             # sur les faces UP & DOWN, il faudra déplacer des lignes uniquement,
             # soit la première soit la dernière en fonction de si la rotation
@@ -127,17 +137,20 @@ class Cube:
 
                 # On choisit arbitrairement de commencer la rotation par la
                 # face UP :
-                # sauvegarde de la face UP :
+                # sauvegarde de la ligne de la face UP amenée à être remplacée
+                # à la fin de la rotation :
                 saveRow = np.copy(self.L[0][idFirstRow])
                 # face LEFT --> face UP (sens de rotation horaire) :
                 self.L[0][idFirstRow] = [self.L[1][k][idFirstColumn] for k in \
                                          range(-1, -4, -1)]
 
-                # sauvegarde de la face RIGHT :
+                # sauvegarde de la colonne de la face RIGHT amenée à être
+                # remplacée à la fin de la rotation :
                 oldRow = np.copy([self.L[3][k][idSecondColumn] for k in \
-                                  range(-1, -4, -1)])
+                                  range(3)])
                 # face UP --> face RIGHT (sens de rotation horaire) :
-                for k in range(-1, -4, -1) :
+                for k in range(3) :
+                    # on conserve l'ordre des listes :
                     self.L[3][k][idSecondColumn] = saveRow[k] 
                 saveRow = oldRow
 
@@ -145,47 +158,110 @@ class Cube:
                 oldRow = np.copy(self.L[5][idSecondRow])
                 # face RIGHT --> face DOWN (sens de rotation horaire) :
                 self.L[5][idSecondRow] = saveRow 
+                for k in range(-1, -4, -1) :
+                    # on inverse l'ordre des listes (-k - 1 varie de 0 à 3) :
+                    self.L[5][idSecondRow][-k - 1] = saveRow[k] 
                 saveRow = oldRow
                 
                 # face DOWN --> face LEFT (sens de rotation horaire) :
-                for k in range(-1, -4, -1) :
-                    self.L[1][k][idFirstColumn] = saveRow[k]                 
+                for k in range(3) :
+                    # on conserve l'ordre des listes :
+                    self.L[1][k][idSecondColumn] = saveRow[k]
             
-            # si ce sont les faces L ou R :
-            elif idFace == 1 or idFace == 3 :                         
-                # TEMPORAIRE, juste là pour que le else ci-dessous ne génère
-                # pas une erreur 'expected an indent block'
-                idRow = 0 
+            # si mouvement est L :
+            elif idFace == 1 :
+                # la couronne est composée de colonnes uniquement :
+                idColumn = 0
+
+                # Les remplacements à effectuer :
+                # face BACK --> face UP                    
+                # face UP --> face FRONT
+                # face FRONT --> face DOWN
+                # face DOWN --> face BACK
+                
+                # On choisit arbitrairement de commencer la rotation par la
+                # face UP :
+                # sauvegarde de la face UP :
+                saveColumn = np.copy([self.L[0][k][idColumn] for k in \
+                                   range(3)])
+                # face BACK --> face UP (sens de rotation horaire) :
+                for k in range(-1, -4, -1) :
+                    # on inverse l'ordre des listes (-k - 1 varie de 0 à 3) :
+                    self.L[0][-k - 1][idColumn] = self.L[4][k][2 - idColumn]
+
+                # même processus pour les remplacements suivants :
+                # UP --> FRONT,
+                # FRONT --> DOWN
+                # en respectant le sens horaire :
+                for j in [2, 5] :
+                    # sauvegarde de la face j
+                    oldColumn = np.copy([self.L[j][k][idColumn] for k in \
+                                   range(3)])
+                                        
+                    for k in range(3) :
+                        # on conserve l'ordre des listes :
+                        self.L[j][k][idColumn] = saveColumn[k]
+                    saveColumn = oldColumn
+
+                # On est obligé de faire le mouvement DOWN --> BACK "à la main",
+                # à cause du décalage d'indice de cette face.
+                for k in range(3):
+                    # on conserve l'ordre des listes :
+                    self.L[4][k][2 - idColumn] = saveColumn[k]                   
+
+                    
+            # si mouvement est R :
+            elif idFace == 3 :
+                # la couronne est composée de colonnes uniquement :
+                idColumn = 2
+                
+                # face FRONT --> face UP
+                # face UP --> face BACK
+                # face BACK --> face DOWN
+                # face DOWN --> face FRONT
+                
+                # On choisit arbitrairement de commencer la rotation par la
+                # face UP :
+                # sauvegarde de la face UP :
+                saveColumn = np.copy([self.L[0][k][idColumn] for k in \
+                                   range(3)])
+                # face FRONT --> face UP (sens de rotation horaire) :
+                for k in range(3) :
+                    # on conserve l'ordre des listes :
+                    self.L[0][k][idColumn] = self.L[2][k][idColumn]
+
+                # sauvegarde BACK :
+                oldColumn = np.copy([self.L[4][k][2 - idColumn] for k in \
+                                   range(3)])
+                # face UP --> face BACK :               
+                for k in range(-1, -4, -1) :
+                    # on inverse l'ordre des listes (-k - 1 varie de 0 à 3) :
+                    self.L[4][-k - 1][2 - idColumn] = saveColumn[k]
+                saveColumn = oldColumn
+
+                # sauvegarde DOWN :
+                oldColumn = np.copy([self.L[5][k][idColumn] for k in \
+                                   range(3)])                
+                # face BACK --> face DOWN :
+                for k in range(-1, -4, -1) :
+                    # on inverse l'ordre des listes (-k - 1 varie de 0 à 3) :
+                    self.L[5][-k - 1][idColumn] = saveColumn[k]
+                saveColumn = oldColumn
+
+                # sauvegarde FRONT :
+                oldColumn = np.copy([self.L[2][k][idColumn] for k in \
+                                   range(3)])                
+                # face DOWN --> face FRONT :
+                for k in range(3) :
+                    # on conserve l'ordre des listes :
+                    self.L[2][k][idColumn] = saveColumn[k]
+
+                ## JUSQU'ICI : TESTÉ ET APPROUVÉ
                 
         else :
             raise TypeError
-    def locate(self, ignoreface, type, dico = {'U':0, 'L':1, 'F':2, 'R':3, 'B':4, 'D':5}):
-        '''
-        ignoreface: str sous la forme (UFD) pour ignorer certaines faces.
-        type: 1: corner 2: les autres
-        Des qu'il en repère 1 il retourne sa position et la pos de sa liaison(?)
-        '''
-        ignoreList = []
-        for i in range(0,len(ignoreface)):
-            idFace = dico[ignoreface[i].upper()]
-            ignoreList.append(idFace)
-        print(ignoreList)
-        j = 0
-        i = 0
-        c = False
-        while c == False:          
-            while i < 3 and not(j in ignoreList):
-                print(self.L[j][i])
-                print('Face',j,"    ",np.where(self.L[j][i]=='O')[0])
-                if len(np.where(self.L[j][i]=='O')[0]) != 0:
-                    c = True
-                    i = 15
-                    print("ZBRA")
-                i = i+1
-            i = 0
-            j = j+1
 
-        
+
 # Exemples :
 # un cube qui permet de voir le déplacement exact de chaque vignette
 # puisqu'elles sont toutes identifiées de manière unique dans la configuration
